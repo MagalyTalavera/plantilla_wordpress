441b18f2020d03546bd290a8e4981d8e744b6708
MagalyTalavera <talaverasanchezmagaly@gmail.com>
2023-04-19 09:43:54 -0500

    subir primera plantilla

---
+++ contenido.php
@@ -0,0 +1,104 @@
+<?php include'include/header.php'?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<div class="container my=3">
+    
+
+    <h1>Tienda de ropas Online</h1> <br>
+
+    
+     <div class="row">
+
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="./img/Mujer comprando ropas.png" alt="Imagen 1" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">Polos</h5>
+             <p class="card-text">Rastrea la tendencia de tus ventas y del crecimiento de tu negocio. ¡Con Shopify es fácil! Con Shopify cuentas con una plataforma para manejar tu negocio en internet con facilidad..</p>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+             <!---<a href="#" class="btn btn-primary">Añadir al carrito</a>--->
+           </div>
+         </div>
+       </div>
+      
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="./img/Mujer comprando ropas.png" alt="Imagen 2" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">Pantalones</h5>
+             <p class="card-text">Rastrea la tendencia de tus ventas y del crecimiento de tu negocio. ¡Con Shopify es fácil! Con Shopify cuentas con una plataforma para manejar tu negocio en internet con facilidad.</p>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+             <!---<a href="#" class="btn btn-primary">Añadir al carrito</a>--->
+           </div>
+         </div>
+       </div>
+      
+      
+      
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="./img/Mujer comprando ropas.png" alt="Imagen 3" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">Zapatillas</h5>
+             <p class="card-text">Rastrea la tendencia de tus ventas y del crecimiento de tu negocio. ¡Con Shopify es fácil! Con Shopify cuentas con una plataforma para manejar tu negocio en internet con facilidad.</p>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+             <!---<a href="#" class="btn btn-primary">Añadir al carrito</a>--->
+           </div>
+         </div>
+        </div>
+        
+       
+     
+     
+    
+      <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="./img/Mujer comprando ropas.png" alt="Imagen 4" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">Busos</h5>
+             <p class="card-text">Rastrea la tendencia de tus ventas y del crecimiento de tu negocio. ¡Con Shopify es fácil! Con Shopify cuentas con una plataforma para manejar tu negocio en internet con facilidad.</p>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+             <!---<a href="#" class="btn btn-primary">Añadir al carrito</a>--->
+           </div>
+         </div>
+       </div>
+      
+       
+      
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="./img/Mujer comprando ropas.png" alt="Imagen 5" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">Cazacas</h5>
+             <p class="card-text">Rastrea la tendencia de tus ventas y del crecimiento de tu negocio. ¡Con Shopify es fácil! Con Shopify cuentas con una plataforma para manejar tu negocio en internet con facilidad.</p>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+             <!---<a href="#" class="btn btn-primary">Añadir al carrito</a>--->
+           </div>
+         </div>
+       </div>
+       
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="./img/Mujer comprando ropas.png" alt="Imagen 6" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">Gorras</h5>
+             <p class="card-text">Rastrea la tendencia de tus ventas y del crecimiento de tu negocio. ¡Con Shopify es fácil! Con Shopify cuentas con una plataforma para manejar tu negocio en internet con facilidad.</p>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+             <!---<a href="#" class="btn btn-primary">Añadir al carrito</a>--->
+           </div>
+         </div>
+       </div>
+       </div> 
+       </div><br>
+ 
+</body>
+</html>
+<?php include'include/footer.php'?>
---
+++ include/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>Mi Pagina Web</title>
+</head>
+<body>
+<header>
+<nav class="navbar bg-success">
+  <div class="container-fluid">
+    <a  href="./contenido2.php"class="navbar-brand">Menu</a>
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+</header>
+</body>
+</html>
